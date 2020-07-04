12eb889a828944d963c63b79bec3f5ec46514ab0
최선재 <homesking@naver.com>
2020-07-04 15:14:11 +0900

    [기능] Login 기능 Stub으로 개발

--- src/main/java/com/direcord/api/LoginApi.java
+++ src/main/java/com/direcord/api/LoginApi.java
@@ -5,17 +5,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping(value = "/")
+@RequestMapping(value = "/login")
 @RestController()
 public class LoginApi {
+	
 	
 //	@PostMapping(path = "/", consumes = "multipart/form-data")
 //		public long index(MultipartFile files) {
 //		return files.getSize();//	}
 	
-	@GetMapping()
-	public String index() {
-		return "성공입니다.";
+	@GetMapping("/signIn")
+	public boolean index(String id, String pwd) {
+		if(id.equalsIgnoreCase("admin") && pwd.equalsIgnoreCase("admin")) {
+			return true;
+		}
+		return false;
 	}
 	
 	@PostMapping()

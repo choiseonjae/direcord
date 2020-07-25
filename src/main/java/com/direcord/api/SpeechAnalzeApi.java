d29691b150d707de8a68269f37e1539a3afec712
최선재 <homesking@naver.com>
2020-07-25 22:59:49 +0900

    화자 구분 api 추가

---
+++ src/main/java/com/direcord/api/SpeechAnalzeApi.java
@@ -0,0 +1,48 @@
+package com.direcord.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.direcord.service.QuickstartSample;
+import com.direcord.service.SpeechSpeakerAnalyst;
+
+@RequestMapping(value = "/speech")
+@RestController()
+public class SpeechAnalzeApi {
+	
+	 private static final Logger logger = LoggerFactory.getLogger(SpeechAnalzeApi.class);
+	 
+	@GetMapping("/wav")
+	public String wav(String fileName) {
+		try {
+			return QuickstartSample.callSTTOfWav(fileName);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "FAIL : " + e.getClass().getSimpleName() + " MSG : "+ e.getMessage();
+		}
+	}
+	
+	@GetMapping("/flac")
+	public String flac(String fileName) {
+		try {
+			return QuickstartSample.callSTTOfFlac(fileName);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "FAIL : " + e.getClass().getSimpleName() + " MSG : "+ e.getMessage();
+		}
+	}
+	
+	@GetMapping("/speaker")
+	public String speaker(String fileName) {
+		try {
+			return SpeechSpeakerAnalyst.getInstance().analyze(fileName);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "FAIL : " + e.getClass().getSimpleName() + " MSG : "+ e.getMessage();
+		}
+	}
+
+}
--- src/main/java/com/direcord/service/SpeechAnalyst.java
+++ src/main/java/com/direcord/service/SpeechAnalyst.java
@@ -1,5 +1,7 @@
 package com.direcord.service;
 
 public interface SpeechAnalyst {
+	
+	public String analyze(String fileName) throws Exception;
 
 }

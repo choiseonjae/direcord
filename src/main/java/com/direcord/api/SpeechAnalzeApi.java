4f0ad2b8082b77cfe771c00967d7fa1bae7e9886
최선재 <homesking@naver.com>
2020-08-05 23:53:42 +0900

    오류 수정 및 의존성 추가

--- src/main/java/com/direcord/api/SpeechAnalzeApi.java
+++ src/main/java/com/direcord/api/SpeechAnalzeApi.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.direcord.model.Speaking;
 import com.direcord.service.SpeechSpeakerAnalyst;
 
 @RestController("/speech")
+@RequestMapping(value = "/speech")
 public class SpeechAnalzeApi {
 
 	private static final Logger logger = LoggerFactory.getLogger(SpeechAnalzeApi.class);

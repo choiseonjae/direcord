483bf753e93a6f7a66e8b5afc49ac51f061e9275
최선재 <homesking@naver.com>
2020-08-06 00:10:04 +0900

    list 더해서 반환

--- src/main/java/com/direcord/api/SpeechAnalzeApi.java
+++ src/main/java/com/direcord/api/SpeechAnalzeApi.java
@@ -18,9 +18,14 @@ public class SpeechAnalzeApi {
 	private static final Logger logger = LoggerFactory.getLogger(SpeechAnalzeApi.class);
 
 	@GetMapping("/speaker/uri")
-	public List<Speaking> speakerUri(String gscUri, int minSpeakerCnt, int maxSpeakerCnt) {
+	public String speakerUri(String gscUri, int minSpeakerCnt, int maxSpeakerCnt) {
 		try {
-			return SpeechSpeakerAnalyst.getInstance().analyzeToUri(gscUri, minSpeakerCnt, maxSpeakerCnt);
+			List<Speaking> list = SpeechSpeakerAnalyst.getInstance().analyzeToUri(gscUri, minSpeakerCnt, maxSpeakerCnt);
+			String str = "";
+			for(Speaking speaking : list) {
+				str += speaking.toString();
+			}
+			return str;
 		} catch (RuntimeException e) {
 			e.printStackTrace();
 		} catch (Exception e) {

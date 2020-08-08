6a4d5d23a98e126c1c7bc8a3e1041b8ac27a6a16
최선재 <homesking@naver.com>
2020-08-08 17:30:34 +0900

    [기능][진행] login api index 메소드 추가

--- src/main/java/com/direcord/api/LoginApi.java
+++ src/main/java/com/direcord/api/LoginApi.java
@@ -8,18 +8,24 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 
-@RequestMapping(value = "/login")
 @RestController()
+@RequestMapping(value = "/login")
 public class LoginApi {
 	@GetMapping("/check/login")
 	public boolean isLogin(String idToken) {
 		try {
 			FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
 			String uid = decodedToken.getUid();
+			System.out.println("[uid] " + uid);
 			return true;
 		} catch (FirebaseAuthException e) {
 			e.printStackTrace();
 			return false;
 		}
+	}
+
+	@GetMapping("/")
+	public boolean index() {
+		return true;
 	}
 }
